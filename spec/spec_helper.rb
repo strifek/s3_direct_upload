626f0f343b23305fe70ab1f24c424093113b7d8a
Piotr Stefański <strifek@gmail.com>
2023-02-24 12:09:03 +0100

    revert direct upload version to 0.1.7 (working version)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ require 'active_support/all'
 require 's3_direct_upload'
 
 RSpec.configure do |config|
+  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
 
